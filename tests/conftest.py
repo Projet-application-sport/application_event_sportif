d68ba0e420ba0b20765dd170d84e85b528a7a01a
Vagrant Default User <vagrant@workstation>
2019-08-27 14:50:53 +0000

    Fix various things all around

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,11 @@ import pytest
 import sys
 
 sys.path.append('.')
-from server import creat_app # a changer des lignes
+from app import create_app # a changer des lignes
 
 @pytest.fixture
 def app():
-    app = creat_app()       # rajouter des lignes 
+    app = create_app()       # rajouter des lignes 
     return app
 
 @pytest.fixture
