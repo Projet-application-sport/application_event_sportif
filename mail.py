a7e3cac31994b86c75ea3cc0c8cdba47fab36682
bouhaza <bouhaza.sofiane@gmail.com>
2019-09-15 16:35:01 +0200

    Update mail.py

--- mail.py
+++ mail.py
@@ -2,29 +2,74 @@
 
 from mailjet_rest import Client
 import os
-api_key = '3c7d767dbc11a8a4b0b53962cd823d23'
-api_secret = '7761428aea9ca5e3e6cab1419c89389b'
+
+# Fonction qui permet d'envoyer un mail suite à l'inscription de l'utilisateur
+# Utilisation de l'API JetMail
+def envoi_mail_inscription(email,prenom):
+    
+    #définir les API_KEY
+    api_key = '3c7d767dbc11a8a4b0b53962cd823d23'
+    api_secret = '7761428aea9ca5e3e6cab1419c89389b'
 
 
-mailjet = Client(auth=(api_key, api_secret), version='v3.1')
-data = {
-    'Messages': [
-      {
-      "From": {
-        "Email": "bouhaza.sofiane@gmail.com",
-        "Name": "Sofiane"
-      },
-      "To": [
+    mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+    data = {
+        'Messages': [
         {
-          "Email": sofiane_mail,
-          "Name": "Sabrina"
+        "From": {
+            "Email": "bouhaza.sofiane@gmail.com",
+            "Name": "Sofiane"
+        },
+        "To": [
+            {
+            "Email": email,
+            "Name": prenom
+            }
+        ],
+        "Subject": "Test Application FootEvent",
+        "TextPart": "Bonjour "+prenom+" , merci de votre inscription. Nous comptons sur vous pour améliorer notre applications. cordialement. ",
+       
+
         }
-      ],
-      "Subject": "Test Application FootEvent",
-      "TextPart": "Bonjour Sabrina, voici un test pour vérifier l'envoi de messages avec l'API mailjet.",
+
+    ]
     }
-  ]
-}
-result = mailjet.send.create(data=data)
-print(result.status_code)
-print(result.json())
+    result = mailjet.send.create(data=data)
+    # print(result.status_code)
+    
+
+#Fonction qui permet d'envoyer le lien lorsque l'utilisateur a oublié son mot de passe
+def envoi_mail_reset_mdp(email):
+    
+    #définir les API_KEY
+    api_key = '3c7d767dbc11a8a4b0b53962cd823d23'
+    api_secret = '7761428aea9ca5e3e6cab1419c89389b'
+    
+    
+    mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+    data = {
+        'Messages': [
+        {
+        "From": {
+            "Email": "bouhaza.sofiane@gmail.com",
+            "Name": "Sofiane"
+        },
+        "To": [
+            {
+            "Email": email,
+            "Name": " "
+            }
+        ],
+        "Subject": "Test Application FootEvent",
+        "HTMLPart": "<h3>Bonjour, Veuillez réinitialiser votre mot de passe en cliquant sur le lien suivant <a href='http://127.0.0.1:5000/mdp_reset'>Mot_de_passe</a>!</h3><br /> Merci, en vous souhaitant une bonne journée"
+
+    
+        }
+
+    ]
+    }
+    result = mailjet.send.create(data=data)
+    # print(result.status_code)
+    
+
+
